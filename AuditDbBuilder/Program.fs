9994ce03c6af66e8d1a3e5e4308b5046207c39ff
mcrews <matthewcrews@gmail.com>
2018-03-26 15:09:03 -0700

    Changed back to yaml
    Figured out how to have empty lists in yaml so I converted back. JSON TypeProvider didn't support the cases I was trying to support

--- AuditDbBuilder/Program.fs
+++ AuditDbBuilder/Program.fs
@@ -2,16 +2,14 @@
 // See the 'F# Tutorial' project for more help.
 
 open System.IO
+//open FSharp.Data
 open FSharp.Configuration
 
-type AuditableDbConfig = YamlConfig<"AuditableDbConfig.yaml">
-type TableConfig = AuditableDbConfig.Config_Type
-type TableDef = AuditableDbConfig.Tables_Item_Type
-type FieldDef = TableDef.Fields_Item_Type
-type TypeMap = AuditableDbConfig.Config_Type.TypeMap_Item_Type
-//type UniqueFields = TableDef.
+type AuditDbConfig = YamlConfig<"AuditableDbConfig.yaml">
+type TableConfig = AuditDbConfig.TableConfig_Type
+type TableDef = AuditDbConfig.Tables_Item_Type
+
 type FieldTypeToSql = string -> string
-type AuditFieldsSet = AuditableDbConfig.Config_Type.AuditFields_Item_Type
 
 let newline = System.Environment.NewLine
 
@@ -22,10 +20,6 @@ let FieldTypeToStringBuilder (d:Map<string, string>) =
             d.[k] +  " NULL"
         else
             d.[s] + " NOT NULL"
-
-let FieldDefToString (m: FieldTypeToSql) (f:FieldDef) =
-    sprintf "[%s] %s" f.Name (m f.Type)
-    
 
 let ComposeFieldsSql (fieldMapper:FieldTypeToSql) (c:TableConfig) (t:TableDef) =
     let indentation = String.replicate c.TableFieldIndentation " "
@@ -40,19 +34,19 @@ let ComposeFieldsSql (fieldMapper:FieldTypeToSql) (c:TableConfig) (t:TableDef) =
         
     Seq.append auditFieldsSql dataFields
 
-let ComposeConstraints (c:TableConfig) (t:TableDef) =
+let ComposeConstraints (c:TableConfig) (t:TableDef) (tableName:string) =
     if t.ForeignKeys.Count > 0 then
         t.ForeignKeys
-        |> Seq.map (fun e -> sprintf "CONSTRAINT FK_%s_%s FOREIGN KEY (%s) REFERENCES [dbo].[%s] (Id)" t.Name e.ForeignTable e.ForeignField e.ForeignTable)
+        |> Seq.map (fun e -> sprintf "%sCONSTRAINT FK_%s_%s FOREIGN KEY (%s) REFERENCES [dbo].[%s] (Id)" (String.replicate c.TableFieldIndentation " ") tableName e.Table e.Field e.Table)
     else
         Seq.empty<string>
 
-let ComposeTableDefSql (fm:FieldTypeToSql) (schema:string) (c:TableConfig) (t:TableDef) =
+let ComposeTableDefSql (fm:FieldTypeToSql) (schema:string) (c:TableConfig) (t:TableDef) (tableName:string) =
     let fieldsSql = ComposeFieldsSql fm c t
-    let constraintsSql = ComposeConstraints c t
+    let constraintsSql = ComposeConstraints c t tableName
     let newlineDelimieter = sprintf ",%s" newline
     let tableSql = String.concat newlineDelimieter (Seq.append fieldsSql constraintsSql)
-    sprintf "CREATE TABLE [%s].[%s]%s(%s%s%s)%sgo%s" schema t.Name newline newline tableSql newline newline newline
+    sprintf "CREATE TABLE [%s].[%s]%s(%s%s%s)%sgo%s" schema tableName newline newline tableSql newline newline newline
 
 let ComposeUniqueConstriantSql (schema:string) (t:TableDef) =
     let newline = System.Environment.NewLine
@@ -62,11 +56,11 @@ let ComposeUniqueConstriantSql (schema:string) (t:TableDef) =
         |> String.concat ", "
     sprintf "CREATE UNIQUE INDEX UIDX_%s on [%s].[%s] (%s);%sgo" t.Name schema t.Name fields newline
 
-let ComposeTableSql (fm:FieldTypeToSql) (c:TableConfig) (schema:string) (t:TableDef) =
-    if (t.Unique.[0] <> "n" && c.AuditSchemaName <> schema) then
-        (ComposeTableDefSql fm schema c t) + newline + (ComposeUniqueConstriantSql schema t)
+let ComposeTableSql (fm:FieldTypeToSql) (c:TableConfig) (schema:string) (t:TableDef) (includeUniqueConst:bool) (tableName:string) =
+    if includeUniqueConst && t.ForeignKeys.Count > 0 then
+        (ComposeTableDefSql fm schema c t tableName) + newline + (ComposeUniqueConstriantSql schema t)
     else
-        ComposeTableDefSql fm schema c t
+        ComposeTableDefSql fm schema c t tableName
 
 let CreateDir d =
     if not(Directory.Exists(d)) then
@@ -96,10 +90,16 @@ let main argv =
     CreateDir auditDir
     CreateDir sequenceDir
 
-    let auditConfig = AuditableDbConfig()
+    let auditConfig = AuditDbConfig()
     auditConfig.Load(configFile)
-    let fieldConverter =
-        auditConfig.Config.TypeMap
+    let tableFieldMapper =
+        auditConfig.TableConfig.TypeMap
+        |> Seq.map (fun e -> e.Type, e.Output)
+        |> Map.ofSeq
+        |> FieldTypeToStringBuilder
+
+    let historyTableFieldMapper =
+        auditConfig.TableConfig.HistoryTableTypeMap
         |> Seq.map (fun e -> e.Type, e.Output)
         |> Map.ofSeq
         |> FieldTypeToStringBuilder
@@ -107,15 +107,15 @@ let main argv =
 
     for t in auditConfig.Tables do
         let tableSqlFile = Path.Combine(tableDir, (t.Name + ".sql"))
-        let tableSql = ComposeTableSql fieldConverter auditConfig.Config "dbo" t
+        let tableSql = ComposeTableSql tableFieldMapper auditConfig.TableConfig auditConfig.TableConfig.OutputSchema t true t.Name
         File.WriteAllText(tableSqlFile, tableSql)
 
-        let auditSqlFile = Path.Combine(auditDir, (t.Name + ".sql"))
-        let auditSql = ComposeTableSql fieldConverter auditConfig.Config auditConfig.Config.AuditSchemaName t
+        let auditSqlFile = Path.Combine(auditDir, (t.Name + auditConfig.TableConfig.AuditTableNameAppend + ".sql"))
+        let auditSql = ComposeTableSql historyTableFieldMapper auditConfig.TableConfig auditConfig.TableConfig.OutputSchema t false (t.Name + auditConfig.TableConfig.AuditTableNameAppend)
         File.WriteAllText(auditSqlFile, auditSql)
 
         let sequenceSqlFile = Path.Combine(sequenceDir, (t.Name + "Ver.sql"))
-        let sequenceSql = ComposeSequenceSql auditConfig.Config.AuditSchemaName t
+        let sequenceSql = ComposeSequenceSql auditConfig.TableConfig.OutputSchema t
         File.WriteAllText(sequenceSqlFile, sequenceSql)    
 
     0 // return an integer exit code
